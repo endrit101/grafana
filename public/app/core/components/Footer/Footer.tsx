99b44d13ac81341110737525ff7a2f5dc8ed5b8c
endrit <endrit.toplica@gmail.com>
2020-08-12 19:05:49 +0200

    Removed 3 bottom sidemenu options

--- public/app/core/components/Footer/Footer.tsx
+++ public/app/core/components/Footer/Footer.tsx
@@ -9,26 +9,27 @@ export interface FooterLink {
   target?: string;
 }
 
+// Removed the three options
 export let getFooterLinks = (): FooterLink[] => {
   return [
-    {
-      text: 'Documentation',
-      icon: 'document-info',
-      url: 'https://grafana.com/docs/grafana/latest/?utm_source=grafana_footer',
-      target: '_blank',
-    },
-    {
-      text: 'Support',
-      icon: 'question-circle',
-      url: 'https://grafana.com/products/enterprise/?utm_source=grafana_footer',
-      target: '_blank',
-    },
-    {
-      text: 'Community',
-      icon: 'comments-alt',
-      url: 'https://community.grafana.com/?utm_source=grafana_footer',
-      target: '_blank',
-    },
+    // {
+    //   text: 'Documentation',
+    //   icon: 'document-info',
+    //   url: 'https://grafana.com/docs/grafana/latest/?utm_source=grafana_footer',
+    //   target: '_blank',
+    // },
+    // {
+    //   text: 'Support',
+    //   icon: 'question-circle',
+    //   url: 'https://grafana.com/products/enterprise/?utm_source=grafana_footer',
+    //   target: '_blank',
+    // },
+    // {
+    //   text: 'Community',
+    //   icon: 'comments-alt',
+    //   url: 'https://community.grafana.com/?utm_source=grafana_footer',
+    //   target: '_blank',
+    // },
   ];
 };
 
